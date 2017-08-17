5b3a0b33112777827bed9ee0606935c76f02365a
Peter Bohning <peter.bohning@gmx.com>
2017-08-16 20:57:52 -0400

    minor

--- driver.cpp
+++ driver.cpp
@@ -6,12 +6,14 @@
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
  *	GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+/* Jan Newger's IDAStealth was helpful for this service manager/driver stuff */
 #include <Windows.h>
 #include <stdio.h>
 #include <string>
