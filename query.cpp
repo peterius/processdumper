653aadb198385dd7fb699bce94494c603598534d
Peter Bohning <peter.bohning@gmx.com>
2017-08-18 22:42:57 -0400

    minor

--- query.cpp
+++ query.cpp
@@ -6,9 +6,9 @@
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- *	GNU General Public License for more details.
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
@@ -20,9 +20,6 @@
 
 int query_from_directory(char * dirname)
 {
-	uint32_t load_address;
-	char * buffer;
-	unsigned long size;
 	int ret;
 	wchar_t * wdirname;
 	size_t converted;
